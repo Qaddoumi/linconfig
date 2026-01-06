69ba4aa373e8860173150ad85c20921dec27a758
Qaddoumi <mqaddumi.7994kohaa@gmail.com>
2026-01-06 14:00:37 +0300

    unblacklist the gpus

--- pkgs/gpu-passthrough.sh
+++ pkgs/gpu-passthrough.sh
@@ -608,12 +608,12 @@ else
 
 fi
 
-echo -e "${green}Blacklist host GPU drivers to prevent automatic binding:${no_color}"
-if [ "$GPU_TYPE" = "nvidia" ]; then
-	echo -e "blacklist nvidia\nblacklist nvidia_drm\nblacklist nvidia_modeset\nblacklist nouveau\nblacklist nvidiafb\nblacklist nvidia_uvm" | sudo tee /etc/modprobe.d/blacklist-nvidia.conf > /dev/null
-elif [ "$GPU_TYPE" = "amdgpu" ]; then
-	echo -e "blacklist amdgpu\nblacklist radeon" | sudo tee /etc/modprobe.d/blacklist-amd.conf > /dev/null
-fi
+# echo -e "${green}Blacklist host GPU drivers to prevent automatic binding:${no_color}"
+# if [ "$GPU_TYPE" = "nvidia" ]; then
+# 	echo -e "blacklist nvidia\nblacklist nvidia_drm\nblacklist nvidia_modeset\nblacklist nouveau\nblacklist nvidiafb\nblacklist nvidia_uvm" | sudo tee /etc/modprobe.d/blacklist-nvidia.conf > /dev/null
+# elif [ "$GPU_TYPE" = "amdgpu" ]; then
+# 	echo -e "blacklist amdgpu\nblacklist radeon" | sudo tee /etc/modprobe.d/blacklist-amd.conf > /dev/null
+# fi
 
 echo -e "${green}Creating VFIO configuration file /etc/modprobe.d/vfio.conf${no_color}"
 echo -e "options vfio-pci ids=$VFIO_IDS" | sudo tee /etc/modprobe.d/vfio.conf > /dev/null
