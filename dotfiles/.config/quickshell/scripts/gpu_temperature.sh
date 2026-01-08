4fc7f63cbd33fabb2933222a33a1bc972280621a
Qaddoumi <mqaddumi.7994kohaa@gmail.com>
2026-01-08 16:05:15 +0300

    fixes

--- dotfiles/.config/quickshell/scripts/gpu_temperature.sh
+++ dotfiles/.config/quickshell/scripts/gpu_temperature.sh
@@ -5,7 +5,7 @@
 get_gpu_temp() {
 	local temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader)
 
-	if [ -z "$temp" ] || [[ "$temp" == *"command not found"* ]]; then
+	if [ -z "$temp" ] || [[ "$temp" == *"command not found"* ]] || [[ "$temp" == *"failed"* ]]; then
 		echo "N/A"
 	else
 		echo "$temp°C"
