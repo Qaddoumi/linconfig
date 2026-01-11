1fd3cf795c5c6c1bf821fc3b632bd5becb28048d
Qaddoumi <mqaddumi.7994kohaa@gmail.com>
2026-01-11 13:57:22 +0300

    fixing gtk themes

--- pkgs/install.sh
+++ pkgs/install.sh
@@ -645,20 +645,9 @@ ls /usr/share/themes/
 echo -e "${green}Available icon and cursor themes:${no_color}"
 ls /usr/share/icons/
 
-# # Additional theming setup
-# gsettings set org.gnome.desktop.interface gtk-theme 'Materia-dark-compact' || true
-# gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' || true
-# gsettings set org.gnome.desktop.interface cursor-theme 'Capitaine-cursors' || true
-# # To get list of options run 'gsettings range org.gnome.desktop.interface color-scheme'
-# gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' || true
-# gsettings set org.gnome.desktop.interface enable-animations false || true
-
-# dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface gtk-theme 'Materia-dark-compact'
-# dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
-# dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface cursor-theme 'Capitaine-cursors'
-# # To get list of options run 'gsettings range org.gnome.desktop.interface color-scheme'
-# dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-# dbus-launch --exit-with-session gsettings set org.gnome.desktop.interface enable-animations false
+## GTK Theming setup
+# do not use gsettings set org.gnome.desktop.interface... as it does not work in chroot
+# and dont use dbus-launch --exit-with-session gsettings... as it does not work in chroot too.
 
 # Create the local schema overrides directory
 sudo mkdir -p /usr/share/glib-2.0/schemas/
